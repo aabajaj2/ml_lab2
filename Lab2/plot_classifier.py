2fe9a6131dd19a248c61bdfab9ce3c7b5ffc4347
Anjani Bajaj <aabajaj2@dons.usfca.edu>
2018-04-12 23:08:04 -0700

    Added normal graph

--- Lab2/plot_classifier.py
+++ Lab2/plot_classifier.py
@@ -1,6 +1,7 @@
 import svm_basic
 
-def plot_fit(fit_line, datamatrix, labelmatrix):
+
+def plot_fit_line(fit_line, datamatrix, labelmatrix):
     import matplotlib.pyplot as plt
     import numpy as np
 
@@ -38,6 +39,37 @@ def plot_fit(fit_line, datamatrix, labelmatrix):
     plt.show()
 
 
+def plot_fit(datamatrix, labelmatrix):
+    import matplotlib.pyplot as plt
+    import numpy as np
+
+    dataarray = np.asarray(datamatrix)
+    n = dataarray.shape[0]
+
+    # Keep track of the two classes in different arrays so they can be plotted later...
+    xcord1 = []
+    ycord1 = []
+    xcord2 = []
+    ycord2 = []
+    for i in range(n):
+        if int(labelmatrix[i]) == 1:
+            xcord1.append(dataarray[i, 0])
+            ycord1.append(dataarray[i, 1])
+        else:
+            xcord2.append(dataarray[i, 0])
+            ycord2.append(dataarray[i, 1])
+    fig = plt.figure()
+
+    # Plot the data as points with different colours
+    ax = fig.add_subplot(111)
+    ax.scatter(xcord1, ycord1, s=30, c='red', marker='s')
+    ax.scatter(xcord2, ycord2, s=30, c='green')
+
+    plt.xlabel('x1')
+    plt.ylabel('x2')
+    plt.show()
+
+
 line = []
 dataMatrix, labelMat = svm_basic.loadDataSet('data/linearly_separable.csv')
 clf = svm_basic.svmClassifier(b=None, alphas=None, C=200, toler=0.0001, maxIter=1000)
@@ -45,8 +77,9 @@ clf = clf.fit(dataMatrix, labelMat)
 print("Classifier after fit", "b=",  clf.b, "Alpha=", clf.alphas)
 hyp = clf.predict(dataMatrix)
 print("Hypothesis=", hyp)
+plot_fit(dataMatrix, labelMat)
 line.append(clf.b.getA()[0])
 line.append(clf.weights[0])
 line.append(clf.weights[1])
-plot_fit(line, dataMatrix, labelMat)
+plot_fit_line(line, dataMatrix, labelMat)
 
